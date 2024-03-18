11ea38ca3ba8997556cd9d6dbe5f7d9b83cd2a91
Amarildo <you@example.com>
2024-03-18 12:53:31 +0100

    added edit route

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,11 @@ Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 // Route per il form
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 
-// Route POST per caricare il nuovo fumetto
-Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
-
 // Route per l'oggetto singolo
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
+
+// Route per la modifica
+Route::get('comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.update');
+
+// Route POST per caricare il nuovo fumetto
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
